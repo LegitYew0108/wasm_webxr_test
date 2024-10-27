3cd21e10948ea31abcb727515b9134f055d36e32
LegitYew0108 <legitlegit0108@gmail.com>
2024-10-27 22:26:33 +0900

    add loop

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 use wasm_bindgen::prelude::*;
+use tracing::{span, Level};
 
-#[wasm_bindgen(start)]
-async fn run() -> Result<(), JsValue>{
+#[wasm_bindgen]
+pub async fn run() -> Result<(), JsValue>{
     let window = web_sys::window().expect("no global `window` exists");
     let document = window.document().expect("should have a document on window");
     let body = document.body().expect("document should have a body");
     let xrsystem = window.navigator().xr();
+    tracing::debug!("xrsystem: {:?}", xrsystem);
     let promise_is_supported = xrsystem.is_session_supported(web_sys::XrSessionMode::ImmersiveVr);
     let is_supported = wasm_bindgen_futures::JsFuture::from(promise_is_supported).await?.is_string();
 
@@ -13,7 +15,19 @@ async fn run() -> Result<(), JsValue>{
     val.set_inner_html(&format!("XR is supported: {}", is_supported));
 
     body.append_child(&val)?;
-    loop {}
 
     Ok(())
+}
+
+#[wasm_bindgen(start)]
+pub async fn init() -> Result<(), JsValue> {
+    let subscriber = tracing_subscriber::fmt()
+        .with_max_level(Level::DEBUG)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+
+    let span = span!(Level::INFO, "init");
+    let _enter = span.enter();
+
+    run().await
 }
