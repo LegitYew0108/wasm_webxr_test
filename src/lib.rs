d98a2dfb2424708dc1a2e3d454f615db7f00b378
LegitYew0108 <legitlegit0108@gmail.com>
2024-10-27 22:42:56 +0900

    removed init_wasm func

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,11 @@
 use wasm_bindgen::prelude::*;
-use tracing::{span, Level};
 
-#[wasm_bindgen]
+#[wasm_bindgen(start)]
 pub async fn run() -> Result<(), JsValue>{
     let window = web_sys::window().expect("no global `window` exists");
     let document = window.document().expect("should have a document on window");
     let body = document.body().expect("document should have a body");
     let xrsystem = window.navigator().xr();
-    tracing::debug!("xrsystem: {:?}", xrsystem);
     let promise_is_supported = xrsystem.is_session_supported(web_sys::XrSessionMode::ImmersiveVr);
     let is_supported = wasm_bindgen_futures::JsFuture::from(promise_is_supported).await?.is_string();
 
@@ -19,15 +17,3 @@ pub async fn run() -> Result<(), JsValue>{
     Ok(())
 }
 
-#[wasm_bindgen(start)]
-pub async fn init_wasm() -> Result<(), JsValue> {
-    let subscriber = tracing_subscriber::fmt()
-        .with_max_level(Level::DEBUG)
-        .finish();
-    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
-
-    let span = span!(Level::INFO, "init");
-    let _enter = span.enter();
-
-    run().await
-}
