8a193b07d2b4d4f845391342c6f08a6ebff9b861
LegitYew0108 <legitlegit0108@gmail.com>
2024-10-27 22:46:04 +0900

    feat: is_supported_webxr represent as bool value

--- src/lib.rs
+++ src/lib.rs
@@ -7,10 +7,14 @@ pub async fn run() -> Result<(), JsValue>{
     let body = document.body().expect("document should have a body");
     let xrsystem = window.navigator().xr();
     let promise_is_supported = xrsystem.is_session_supported(web_sys::XrSessionMode::ImmersiveVr);
-    let is_supported = wasm_bindgen_futures::JsFuture::from(promise_is_supported).await?.is_string();
+    let is_supported = wasm_bindgen_futures::JsFuture::from(promise_is_supported).await?.as_bool();
 
     let val = document.create_element("p")?;
-    val.set_inner_html(&format!("XR is supported: {}", is_supported));
+    match is_supported {
+        Some(true) => val.set_class_name("supported"),
+        Some(false) => val.set_class_name("not-supported"),
+        None => val.set_class_name("unknown"),
+    };
 
     body.append_child(&val)?;
 
