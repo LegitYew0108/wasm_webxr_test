29e8f541b5812204b453746b060527289af79020
LegitYew0108 <legitlegit0108@gmail.com>
2024-10-27 21:33:27 +0900

    fix: deal with Promise

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,16 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(start)]
-fn run() -> Result<(), JsValue>{
+async fn run() -> Result<(), JsValue>{
     let window = web_sys::window().expect("no global `window` exists");
     let document = window.document().expect("should have a document on window");
     let body = document.body().expect("document should have a body");
     let xrsystem = window.navigator().xr();
-    let is_supported = xrsystem.is_session_supported(web_sys::XrSessionMode::ImmersiveVr).as_string();
+    let promise_is_supported = xrsystem.is_session_supported(web_sys::XrSessionMode::ImmersiveVr);
+    let is_supported = wasm_bindgen_futures::JsFuture::from(promise_is_supported).await?.is_string();
 
     let val = document.create_element("p")?;
-    match is_supported{
-        Some(supported) => {
-            val.set_inner_html(&format!("XR is supported: {}", supported));
-        },
-        None => {
-            val.set_inner_html("None");
-        }
-    }
+    val.set_inner_html(&format!("XR is supported: {}", is_supported));
 
     body.append_child(&val)?;
 
